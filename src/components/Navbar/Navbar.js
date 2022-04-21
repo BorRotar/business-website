502f7a557364c2596a1d866b098d055427feb089
BorRotar <bor.rotar1@hotmail.com>
2022-04-21 22:15:04 +0200

    Added pill button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="fixed flex-column top-60 left-60 shadow-sm bg-emerald-400 w-100">
-      <span className="basis-1/2 relative shadow-xl font-mono">
-        <button>
+    <div className="fixed flex-column top-60 left-60 w-100">
+      <span className="rela tive shadow-xl font-mono">
+        <button className="basis-1/2 shadow-sm bg-emerald-400 rounded-full border-2">
           <Link to="/">Vision</Link>
         </button>
-        <button className="basis-1/2 relative shadow-xl">
+        <button className="basis-1/2 shadow-sm bg-emerald-400 rounded-full border-2">
           <Link to="/about">About</Link>
         </button>
       </span>
