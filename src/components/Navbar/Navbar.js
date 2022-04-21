a3524c6933d2ffe0535f37588578a956c2a11440
BorRotar <bor.rotar1@hotmail.com>
2022-04-21 23:02:16 +0200

    Updated positions

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="fixed flex-column top-60 left-60 w-100">
-      <span className="rela tive shadow-xl font-mono">
-        <button className="basis-1/2 shadow-sm bg-emerald-400 rounded-full border-2">
+    <div className="relative h-62 w-62">
+    <div className="flex absolute inset-x-0 top-40 left-40 h-16">
+      <span className="text-2xl rounded-full border-3 relative grid grid-cols-2 divide-x bg-emerald-400 shadow-xl font-mono">
+        <button className="shadow-sm px-8">
           <Link to="/">Vision</Link>
         </button>
-        <button className="basis-1/2 shadow-sm bg-emerald-400 rounded-full border-2">
+        <button className="shadow-sm px-8">
           <Link to="/about">About</Link>
         </button>
       </span>
+    </div>
     </div>
   );
 };
