f649952064547af0363ad471b1891e269aad17e0
BorRotar <bor.rotar1@hotmail.com>
2022-04-22 18:33:19 +0200

    About css grid with hover

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,50 @@
+import { useState } from "react";
 import Sakura from "../../assets/images/SakuraContrast.jpg";
 
 const About = () => {
+ const [hover, setHover] = useState(null)
+ const onHover = () => {
+   setHover(true);
+ };
+ const onLeave = () => {
+  setHover(false);
+};
+const [hhover, setHhover] = useState(null)
+const onHhover = () => {
+  setHhover(true);
+};
+const onLleave = () => {
+ setHhover(false);
+};
+
   return (
-    <div>
-      <img className="absolute top-80 left-80 w-24 h-24 rounded-full mx-auto" src={Sakura} alt='Beautiful sakura trees'/>
-    </div>
+    <div className="relative top-80 left-5 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
+ <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8">
+  <div onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+  >
+    {hover ? 'Visible text' : 
+    <img src={Sakura} alt="" />}
+  </div>
+  <div className="col-start-3">
+  <div onMouseEnter={onHhover}
+      onMouseLeave={onLleave}
+  >
+    {hhover ? 'Visible text' : 
+    <img src={Sakura} alt="" />}
+  </div>
+  </div>
+  <div>
+    <img src={Sakura} alt=""/>
+  </div>
+  <div>
+    <img src={Sakura} alt=""/>
+  </div>
+  <div className="row-start-1 col-start-2 col-span-2">
+    <img src={Sakura} alt=""/>
+  </div>
+</div>
+</div>
   );
 };
 
--- src/components/Vision/Vision.js
+++ src/components/Vision/Vision.js
@@ -1,10 +1,25 @@
-
+import { Fragment } from "react";
+import Sakura from "../../assets/images/SakuraContrast.jpg";
 
 const Vision = () => {
-    return (<div className="shadow-2xl  px-60 py-80">
-        <p>This is my vision</p>
-        </div>
-        );
-}
+  return (
+      <Fragment>
+    <div className="container w-96 absolute  top-80 left-20">
+      <p className="card text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+    <div className="container absolute bottom-64 left-60">
+    <img className="w-96 rounded-full mx-auto" src={Sakura} alt='Beautiful sakura trees'/>
+    </div>
+    </Fragment>
+  );
+};
 
-export default Vision
+export default Vision;
