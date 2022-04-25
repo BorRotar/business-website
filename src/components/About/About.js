9f9520db0a9a6f4001c53a48f8697ed54aade622
BorRotar <bor.rotar1@hotmail.com>
2022-04-25 12:52:46 +0200

    Limited columns to 4

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -43,33 +43,29 @@ const About = () => {
   };
 
   return (
-    <div className="relative top-80 left-5 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
-      <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8 resize-none">
-        <div className="relative top-0 left-0" onMouseEnter={onFirstHover} onMouseLeave={onFirstLeave}>
+    <div className="grow-0 relative top-80 grid grid-cols-4 gap-4">
+     
+        <div  onMouseEnter={onFirstHover} onMouseLeave={onFirstLeave}>
           {firstHover ? <div className="">{<Projects />}</div> : <img src={Sakura} alt="" />}
         </div>
-        <div className="col-start-3 max-w-md">
-          <div className="position-absolute" onMouseEnter={onSeocndHover} onMouseLeave={onSecondLeave}>
-            {secondHover ? <div className="w-30">{secondProject}</div> : <img src={Sakura} alt="" />}
+        <div >
+          <div className="" onMouseEnter={onSeocndHover} onMouseLeave={onSecondLeave}>
+            {secondHover ? <div className="">{secondProject}</div> : <img src={Sakura} alt="" />}
           </div>
         </div>
-        <div>
+        <div className="overflow-hidden">
           <div onMouseEnter={onThirdHover} onMouseLeave={onThirdLeave}>
-            {thirdHover ? <div className="w-30">{thirdProject}</div> : <img src={Sakura} alt="" />}
+            {thirdHover ? <div className="">{thirdProject}</div> : <img src={Sakura} alt="" />}
           </div>
         </div>
         <div>
           <div onMouseEnter={onFourthHover} onMouseLeave={onFourthLeave}>
-            {fourthHover ? <div className="max-w-prose">{fourthProject}</div> : <img src={Sakura} alt="" />}
+            {fourthHover ? <div className="">{fourthProject}</div> : <img src={Sakura} alt="" />}
           </div>
         </div>
-        <div className="row-start-1 col-start-2 col-span-2 max-h-8 resize-none">
-          <div className="position-absolute" onMouseEnter={onFifthHover} onMouseLeave={onFifthLeave}>
-            {fifthHover ? <div className="">{fifthProject}</div> : <img className="" src={Sakura} alt="" />}
-          </div>
-        </div>
+
       </div>
-    </div>
+
   );
 };
 
