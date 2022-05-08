acadb829c69c0fe13b31d05d00baf094ab4ca471
BorRotar <bor.rotar1@hotmail.com>
2022-05-08 10:20:37 +0200

    Completed Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { Fragment, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Cropped from "../../assets/images/CroppedSpace.jpg";
 
 const Contact = () => {
   const form = useRef();
@@ -7,21 +8,33 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_rfpu89s', 'template_r9362sc', form.current, 'Am-qtnqXH6IAnbHiG')
-      .then(() => {
+    emailjs
+      .sendForm(
+        "service_rfpu89s",
+        "template_r9362sc",
+        form.current,
+        "Am-qtnqXH6IAnbHiG"
+      )
+      .then(
+        () => {
           alert("Message sent successfully!");
-          window.location.reload(false)
-      }, () => {
+          window.location.reload(false);
+        },
+        () => {
           alert("Something went wrong, please try again");
-      });
-    }
-    return (
-<Fragment>
-<div className="relative top-96 flex w-full justify-center w-100">
-      <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md w-1/3">
-  <form ref={form} onSubmit={sendEmail}>
-    <div className="form-group mb-6">
-      <input type="text" name='from_name' className="form-control block
+        }
+      );
+  };
+  return (
+    <Fragment>
+      <div className="relative top-96 flex w-full justify-center w-100">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md w-1/3">
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="form-group mb-6">
+              <input
+                type="text"
+                name="from_name"
+                className="form-control block
         w-full
         px-3
         py-1.5
@@ -34,11 +47,16 @@ const Contact = () => {
         transition
         ease-in-out
         m-0
-        focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none" id="exampleInput7"
-        placeholder="Name" />
-    </div>
-    <div className="form-group mb-6">
-      <input type="email" name='from_email' className="form-control block
+        focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none"
+                id="exampleInput7"
+                placeholder="Name"
+              />
+            </div>
+            <div className="form-group mb-6">
+              <input
+                type="email"
+                name="from_email"
+                className="form-control block
         w-full
         px-3
         py-1.5
@@ -51,13 +69,15 @@ const Contact = () => {
         transition
         ease-in-out
         m-0
-        focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none" id="exampleInput8"
-        placeholder="Email address" />
-    </div>
-    <div className="form-group mb-6">
-      <textarea
-      name='message'
-      className="
+        focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none"
+                id="exampleInput8"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="form-group mb-6">
+              <textarea
+                name="message"
+                className="
         form-control
         block
         w-full
@@ -74,12 +94,15 @@ const Contact = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none
       "
-      id="exampleFormControlTextarea13"
-      rows="3"
-      placeholder="Message"
-    ></textarea>
-    </div>
-    <button type="submit" value="Send" className="
+                id="exampleFormControlTextarea13"
+                rows="3"
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              value="Send"
+              className="
       w-full
       px-6
       py-2.5
@@ -96,38 +119,46 @@ const Contact = () => {
       active:bg-emerald-800 active:shadow-lg
       transition
       duration-150
-      ease-in-out">Send</button>
-  </form>
- 
-
-</div>
-
-</div>
-<div className="relative top-96 left-96 flex w-full justify-center w-100">
-  <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 w-96">
-  <img className="w-24 h-24 rounded-full" src="/sarah-dayan.jpg" alt="" width="384" height="512"/>
-  <div className="pt-6 space-y-4">
-    <blockquote>
-      <p className="text-lg">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption>
-      <div>
-        Sarah Dayan
+      ease-in-out"
+            >
+              Send
+            </button>
+          </form>
+        </div>
       </div>
-      <div>
-        Staff Engineer, Algolia
+      <div className="relative top-96 md:left-4 xl:left-96 flex w-full justify-center w-100">
+        <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 w-96">
+          <img
+            className="w-24 h-24 rounded-full"
+            src={Cropped}
+            alt=""
+            width="384"
+            height="512"
+          />
+          <div className="pt-6 space-y-4">
+            <blockquote>
+              <p className="text-lg">
+                Asdf Ffff
+              </p>
+              <p className="text-lg">
+                Address 34
+              </p>
+              <p className="text-lg">
+              email@email
+              </p>
+              <p className="text-lg">
+              123 123 123 123
+              </p>
+            </blockquote>
+            <figcaption>
+              <div>Consulting LLC</div>
+              <div>Ljubljana, Slovenia</div>
+            </figcaption>
+          </div>
+        </figure>
       </div>
-    </figcaption>
-  </div>
-</figure></div>
+    </Fragment>
+  );
+};
 
-
-</Fragment>
-    )
-}
-
-export default Contact
+export default Contact;
--- src/components/Vision/Vision.js
+++ src/components/Vision/Vision.js
@@ -1,24 +1,28 @@
-import { Fragment } from "react";
 import Sakura from "../../assets/images/SakuraContrast.jpg";
 
 const Vision = () => {
   return (
-      <Fragment>
-    <div className="container w-96 absolute  top-80 left-20">
-      <p className="card text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <div className="absolute flex mb-4 top-80 left-4 right-4 gap-8">
+      <div className="w-1/2 flex justify-center flex-wrap">
+        <h1 className="card text-justify w-96">Extremely successfull business</h1>
+        <p className="card text-justify w-96 ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className="w-1/2">
+        <img
+          className=" rounded-full mx-auto"
+          src={Sakura}
+          alt="Beautiful sakura trees"
+        />
+      </div>
     </div>
-    <div className="container relative float-rigth top-80">
-    <img className="w-96 rounded-full mx-auto" src={Sakura} alt='Beautiful sakura trees'/>
-    </div>
-    </Fragment>
   );
 };
 
